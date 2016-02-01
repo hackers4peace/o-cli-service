a7ab522b268b3874f7aee985cac49f1a1deea288
elf Pavlik <perpetual-tripper@wwelves.org>
2016-01-31 21:01:20 -0600

    added tasks - idp:add:key and idp:add:profile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,8 @@ import { promises as jsonld } from 'jsonld'
 import _ from 'lodash'
 import uuid from 'uuid'
 
+const PLP_CONTEXT = 'https://w3id.org/plp/v1'
+
 function put(db, uri, doc) {
   return jsonld.normalize(doc, { algorithm: 'URDNA2015', format: 'application/nquads' })
     .then((normalized) => {
@@ -15,6 +17,21 @@ function put(db, uri, doc) {
         uri: uri,
         hash: hash
       }
+    })
+}
+
+function get(db, uri, context) {
+  return db.get(uri)
+    .then((normalized) => {
+      return jsonld.fromRDF(normalized, { format: 'application/nquads' })
+    }).then((expanded) => {
+      if (context) {
+        return jsonld.compact(expanded, context)
+      } else {
+        return expanded
+      }
+    }).then((json) => {
+      return json
     })
 }
 
@@ -56,11 +73,9 @@ gulp.task('db:get', () => {
   console.log('db:get uri: ', argv.uri)
   let config = JSON.parse(fs.readFileSync(argv.config, 'utf8'))
   let db = new Storage(config.db)
-  db.get(argv.uri)
-    .then((doc) => {
-      return jsonld.fromRDF(doc, { format: 'application/nquads' })
-    }).then((expanded) => {
-      return console.log(JSON.stringify(expanded))
+  get(db, argv.uri, argv.context)
+    .then((json) => {
+      return console.log(JSON.stringify(json))
     }).catch((err) => {
       console.log(err)
     })
@@ -92,10 +107,10 @@ gulp.task('idp:new', () => {
   console.log('idp:new name: ', argv.name)
   let config = JSON.parse(fs.readFileSync(argv.config, 'utf8'))
   let db = new Storage(config.db)
-  let doc = { '@context': 'https://w3id.org/plp/v1' }
+  let doc = { '@context': PLP_CONTEXT }
   let baseUri = config.hapi.baseUri + uuid.v4()
   let id = baseUri + '#id'
-    let profile = {
+  let profile = {
     "id": id,
     "type": [ "foaf:Person", "schema:Person", "as:Person" ],
     "name": argv.name
@@ -119,12 +134,85 @@ gulp.task('idp:new', () => {
     })
 })
 
+/*
+ * TODO:
+ * * validate if in service namespace = config.hapi.baseUri
+ * * validate if not already existing
+ */
+gulp.task('idp:add:key', () => {
+  console.log('idp:add:key config: ', argv.config)
+  console.log('idp:add:key identity: ', argv.identity)
+  console.log('idp:add:key pem: ', argv.pem)
+  let config = JSON.parse(fs.readFileSync(argv.config, 'utf8'))
+  let pub = fs.readFileSync(argv.pem, 'utf8')
+  let db = new Storage(config.db)
+  let doc = { '@context': PLP_CONTEXT }
+  let baseUri = argv.identity.replace('#id', '/')
+  let key = {
+    "id": baseUri + uuid.v4() + '#id',
+    "type": "sec:Key",
+    "sec:owner": argv.identity,
+    "sec:publicKeyPem": pub
+
+  }
+  doc[key.id.replace('#id', '')] = [ key ]
+  get(db, argv.identity.replace('#id', ''), PLP_CONTEXT)
+    .then((json) => {
+      return _.find(json['@graph'], (obj) => {
+        return obj.rel === 'sec:publicKey'
+      })
+    }).then((link) => {
+      return get(db, link.id, PLP_CONTEXT)
+    }).then((container) => {
+      if (!container['ldp:member']) {
+        container['ldp:member'] = []
+      } else if (typeof(container['ldp:member'] === 'string')) {
+        container['ldp:member'] = [ container['ldp:member'] ]
+      }
+      container['ldp:member'].push(key.id)
+      doc[container.id] = [ container ]
+      return imp(db, doc)
+    }).then((arr) => {
+      return arr.forEach((result) => {
+        console.log('idp:add:key uri: ', result.uri)
+        console.log('idp:add:key hash:', result.hash)
+      })
+    }).catch((err) => {
+      console.log(err)
+    })
+})
+
+gulp.task('idp:add:profile', () => {
+  console.log('idp:add:profile config: ', argv.config)
+  console.log('idp:add:profile identity: ', argv.identity)
+  console.log('idp:add:profile profile: ', argv.profile)
+  let config = JSON.parse(fs.readFileSync(argv.config, 'utf8'))
+  let db = new Storage(config.db)
+  let doc = { '@context': PLP_CONTEXT }
+  let profile = {
+    "id": argv.profile,
+    "type": [ "foaf:ProfileDocument", "as:Profile" ],
+    "as:describes": argv.identity
+
+  }
+  let uri = argv.identity.replace('#id', '')
+  get(db, uri, PLP_CONTEXT)
+    .then((json) => {
+      json['@graph'].push(profile)
+      return put(db, uri, json)
+    }).then((result) => {
+      return console.log('idp:add:profile hash:', result.hash)
+    }).catch((err) => {
+      console.log(err)
+    })
+})
+
 gulp.task('ws:new', () => {
   console.log('ws:new config: ', argv.config)
   console.log('ws:new identity: ', argv.identity)
   let config = JSON.parse(fs.readFileSync(argv.config, 'utf8'))
   let db = new Storage(config.db)
-  let doc = { '@context': 'https://w3id.org/plp/v1' }
+  let doc = { '@context': PLP_CONTEXT }
   let baseUri = config.hapi.baseUri + uuid.v4()
   let profile = {
     "id": baseUri + '/' + uuid.v4(),
